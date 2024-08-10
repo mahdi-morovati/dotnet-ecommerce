d9db1f17c6754e00af1af4452dac39891666f59f
mahdi morovati <morovati155@gmail.com>
2024-08-10 16:49:20 +0330

    create mapping, repository

---
+++ ShopManagement.Infrastructure.EFCore/Repository/SlideRepository.cs
@@ -0,0 +1,42 @@
+using _0_framework.Infrastructure;
+using ShopManagement.Application.Contracts.Slide;
+using ShopManagement.Domain.SlideAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Repository;
+
+public class SlideRepository : RepositoryBase<long, Slide>, ISlideRepository
+{
+    private readonly ShopContext _context;
+
+    public SlideRepository(ShopContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public EditSlide GetDetails(long id)
+    {
+        return _context.Slides.Select(x => new EditSlide
+        {
+            Id = id,
+            BtnText = x.BtnText,
+            Heading = x.Heading,
+            Picture = x.Picture,
+            PictureTitle = x.PictureTitle,
+            PictureAlt = x.PictureAlt,
+            Title = x.Title,
+            Text = x.Text,
+        }).FirstOrDefault(x => x.Id == id);
+    }
+
+    public List<SlideViewModel> GetList()
+    {
+        return _context.Slides.Select(x => new SlideViewModel
+        {
+            Id = x.Id,
+            Heading = x.Heading,
+            Picture = x.Picture,
+            Title = x.Title,
+            CreationDate = x.CreationDate.ToString()
+        }).OrderByDescending(x => x.Id).ToList();
+    }
+}
