9563cfb93c9e09eead891da62298081c75d0be2c
mahdi morovati <morovati155@gmail.com>
2024-08-17 16:03:32 +0330

    creating search and menu

--- 01_LampshadeQuery/Contracts/Product/IProductQuery.cs
+++ 01_LampshadeQuery/Contracts/Product/IProductQuery.cs
@@ -3,4 +3,5 @@ namespace _01_LampshadeQuery.Contracts.Product;
 public interface IProductQuery
 {
     List<ProductQueryModel> GetLatestArrivals();
+    List<ProductQueryModel> Search(string value);
 }
