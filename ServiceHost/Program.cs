459ea5977d97c5b800d2f31f784564dcdd1bb66d
Mahdi Morovati <morovati155@gmail.com>
2024-09-26 12:58:59 +0330

    AddTransient

--- ServiceHost/Program.cs
+++ ServiceHost/Program.cs
@@ -1,4 +1,6 @@
 using _0_framework.Application;
+using _0_framework.Application.Sms;
+using _0_framework.Application.ZarinPal;
 using _0_framework.Infrastructure;
 using AccountManagement.Configuration;
 using DiscountManagement.Configuration;
@@ -21,6 +23,11 @@ AccountManagementBootstrapper.Configure(builder.Services, connectionString);
 builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
 builder.Services.AddTransient<IFileUploader, FileUploader>();
 builder.Services.AddTransient<IAuthHelper, AuthHelper>();
+builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+builder.Services.AddTransient<IFileUploader, FileUploader>();
+builder.Services.AddTransient<IAuthHelper, AuthHelper>();
+builder.Services.AddTransient<IZarinPalFactory, ZarinPalFactory>();
+builder.Services.AddTransient<ISmsService, SmsService>();
 
 builder.Services.Configure<CookiePolicyOptions>(options =>
 {
