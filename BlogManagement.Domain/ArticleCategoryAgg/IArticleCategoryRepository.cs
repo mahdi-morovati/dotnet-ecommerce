18b354b3296fd29300ef5f2ef70178a89befc222
Mahdi Morovati <morovati155@gmail.com>
2024-10-13 13:54:46 +0330

    create ArticleCategoryRepository.cs

--- BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
+++ BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -1,6 +1,11 @@
+using BlogManagement.Application.Contracts.ArticleCategory;
+
 namespace BlogManagement.Domain.ArticleCategoryAgg;
 
 public interface IArticleCategoryRepository
 {
-    
+    string? GetSlugBy(long id);
+    EditArticleCategory? GetDetails(long id);
+    List<ArticleCategoryViewModel> GetArticleCategories();
+    List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
 }
